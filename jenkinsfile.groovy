21ebc270b76a03ba3be24a920a1385e3f0f61b43
andes-noh <nkj9693@gmail.com>
2022-08-24 13:58:25 +0900

    chore(jenkins): jenkins 파일 추가

---
+++ jenkinsfile.groovy
@@ -0,0 +1,44 @@
+pipeline {
+    agent any
+
+    environment {
+        GIT_URL = "https://github.com/andes-noh/CI-CD-TEST.git"
+        dockerHubRegistry = 'andesnoh/sample'
+        dockerHubRegistryCredential = 'dockerhub'
+    }
+
+    tools {
+        nodejs "NodeJS"
+    }
+
+    stages {
+        stage('Pull') {
+            steps {
+                git url: "${GIT_URL}", branch: "main", poll: true, changelog: true
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh "docker build -t ${dockerHubRegistry}:latest"
+            }
+        }
+
+        stage('Push') {
+            steps {
+              docker.withRegistry('https://registry.hub.docker.com', 'dockerhub')
+                sh "docker push ${dockerHubRegistry}:latest"
+
+                sleep 10
+            }
+        }
+
+       stage('Finish') {
+            steps{
+              script{
+                kubernetesDeploy(configs: "jenkins_deploy.yaml", kubeconfigId: "KubeConfig")
+              }
+            }
+        }
+    }
+}
